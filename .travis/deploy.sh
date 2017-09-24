e6fd0f2615246198ae8dc6a13d685dade62795ee
Vlad Kolotov <vkolotov@gmail.com>
2017-09-24 23:48:38 +1300

    Fixing travis build (removing m2 cache dir + enabling snapshot uploading) Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,5 +6,5 @@ then
     mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$VERSION 1>/dev/null 2>/dev/null
     mvn deploy -P sign,build-extras --settings .travis/settings.xml
 else
-    mvn deploy -P !build-extras --settings .travis/settings.xml
+    mvn deploy -Dtravis=true -P !build-extras --settings .travis/settings.xml
 fi
