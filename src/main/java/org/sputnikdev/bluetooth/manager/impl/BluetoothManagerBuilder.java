cdef4cc3b58e25f4993a17d3552462316a9dc45b
Vlad Kolotov <vkolotov@gmail.com>
2018-01-23 22:20:14 +1300

    Improving bluetooth manager builder. Fixes https://github.com/sputnikdev/bluetooth-manager/issues/7 Signed-off-by: Vlad Kolotov <vkolotov@gmail.com>

--- src/main/java/org/sputnikdev/bluetooth/manager/impl/BluetoothManagerBuilder.java
+++ src/main/java/org/sputnikdev/bluetooth/manager/impl/BluetoothManagerBuilder.java
@@ -25,6 +25,9 @@ import org.sputnikdev.bluetooth.manager.BluetoothManager;
 import org.sputnikdev.bluetooth.manager.DeviceDiscoveryListener;
 import org.sputnikdev.bluetooth.manager.DiscoveredAdapter;
 import org.sputnikdev.bluetooth.manager.DiscoveredDevice;
+import org.sputnikdev.bluetooth.manager.transport.BluetoothObjectFactory;
+
+import java.lang.reflect.Constructor;
 
 /**
  * Bluetooth Manager instance builder.
@@ -38,6 +41,10 @@ public class BluetoothManagerBuilder {
     private int refreshRate = BluetoothManagerImpl.REFRESH_RATE_SEC;
     private boolean combinedAdapters;
     private boolean combinedDevices = true;
+    private boolean tinybTransport;
+    private String bluegigaRegex;
+    private boolean started = true;
+    private boolean discovering;
 
     /**
      * Sets how frequent the discovery process should update its state.
@@ -82,12 +89,62 @@ public class BluetoothManagerBuilder {
     }
 
     /**
-     * If set to true all discovered devices with the same address (but discovered through different adapters)
+     * If set to true, all discovered devices with the same address (but discovered through different adapters)
      * are combined into a single device and therefore can be controlled as a single unit.
      * @param combinedDevices if true, all discovered devices with the same address are combined into a single device
      */
     public BluetoothManagerBuilder withCombinedDevices(boolean combinedDevices) {
         this.combinedDevices = combinedDevices;
+        return this;
+    }
+
+    /**
+     * Is set to true, TinyB transport is registered (if found in the classpath).
+     * @param tinybTransport register TinyB transport
+     */
+    public BluetoothManagerBuilder withTinyBTransport(boolean tinybTransport) {
+        this.tinybTransport = tinybTransport;
+        return this;
+    }
+
+    /**
+     * If the provided argument is not null, then BlueGiga is registered with the provided regular expression
+     * for BlueGiga adapters (serial ports) auto discovery.
+     * WARNING!: It is very important not to make the regular expression too wide so that ONLY Bluegiga
+     * adapters/ serial ports are matched by the regular expression. If the regular expression is too broad,
+     * this can lead to hardware malfunction/damage of other devices that are accidentally matched by the
+     * regular expression. USE THIS FEATURE AT YOUR OWN RISK.
+     * <br>Regular expression examples:
+     * <ul>
+     *  <li>Defining some specific serial ports (preferable option): (/dev/ttyACM1)|(/dev/ttyACM2)</li>
+     *  <li>Matching all serial ports on Linux: ((/dev/ttyACM)[0-9]{1,3})</li>
+     *  <li>Matching all serial ports on OSX: (/dev/tty.(usbmodem).*)</li>
+     *  <li>Matching all serial ports on Windows: ((COM)[0-9]{1,3})</li>
+     *  <li>Matching all serial ports with some exclusions: (?!/dev/ttyACM0|/dev/ttyACM5)((/dev/ttyACM)[0-9]{1,3})</li>
+     *  <li>Default regular expression is to match nothing: (?!)</li>
+     * </ul>
+     * @param tinybTransport register BlueGiga transport
+     */
+    public BluetoothManagerBuilder withBlueGigaTransport(String bluegigaRegex) {
+        this.bluegigaRegex = bluegigaRegex;
+        return this;
+    }
+
+    /**
+     * If set to true, bluetooth manager will be started.
+     * @param started if true, bluetooth manager will be started
+     */
+    public BluetoothManagerBuilder withStarted(boolean started) {
+        this.started = started;
+        return this;
+    }
+
+    /**
+     * If set to true, the discovery process will be enabled.
+     * @param started if true, the discovery process will be enabled
+     */
+    public BluetoothManagerBuilder withDiscovering(boolean discovering) {
+        this.discovering = discovering;
         return this;
     }
 
@@ -102,7 +159,41 @@ public class BluetoothManagerBuilder {
         manager.setRefreshRate(refreshRate);
         manager.enableCombinedAdapters(combinedAdapters);
         manager.enableCombinedDevices(combinedDevices);
+        if (tinybTransport) {
+            loadTinyBTransport(manager);
+        }
+        if (bluegigaRegex != null) {
+            loadBlueGigaTransport(manager);
+        }
+        if (started) {
+            manager.start(discovering);
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(manager::dispose));
+
         return manager;
+    }
+
+    private void loadTinyBTransport(BluetoothManager bluetoothManager) {
+        try {
+            Class<?> tinybFactoryClass =
+                    Class.forName("org.sputnikdev.bluetooth.manager.transport.tinyb.TinyBFactory");
+            tinybFactoryClass.getDeclaredMethod("loadNativeLibraries").invoke(tinybFactoryClass);
+            bluetoothManager.registerFactory((BluetoothObjectFactory) tinybFactoryClass.newInstance());
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private void loadBlueGigaTransport(BluetoothManager bluetoothManager) {
+        try {
+            Class<?> bluegigaFactoryClass =
+                    Class.forName("org.sputnikdev.bluetooth.manager.transport.bluegiga.BluegigaFactory");
+            Constructor<?> constructor = bluegigaFactoryClass.getConstructor(String.class);
+            bluetoothManager.registerFactory((BluetoothObjectFactory) constructor.newInstance(bluegigaRegex));
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
     }
 
 }
